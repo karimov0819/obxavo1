058177c379a6a36af5568391421a0f8a63fa3bc0
karimov0819 <karimovmuhammdrizo2009@gmail.com>
2024-07-20 11:11:56 +0500

    salom

---
+++ obxavo1/android/app/src/main/kotlin/com/example/obxavo1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.obxavo1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ obxavo1/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: A(),
+  ));
+}
+
+class A extends StatefulWidget {
+  const A({super.key});
+
+  @override
+  State<A> createState() => _AState();
+}
+
+class _AState extends State<A> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text("data"));
+  }
+}
